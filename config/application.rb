8c93e231f3c28302ba2cd46683d48fe4003f6146
Alirio Diaz <silverneonstaring@gmail.com>
2016-01-12 15:21:48 -0430

    config assets enabled

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module Diasan
     config.active_record.raise_in_transactional_callbacks = true
     config.serve_static_assets = true
     config.assets.initialize_on_precompile = false
+    config.assets.enabled = true
   end
 end
