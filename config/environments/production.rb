5e0f7e5f67ec32912188a81b5bc3338f929b8362
Alirio Diaz <silverneonstaring@gmail.com>
2016-01-12 17:08:42 -0430

    repair mailer production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,26 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+
+  config.time_zone = "Caracas"
+
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "contactoelectrificacionesdiasa@gmail.com",
+    password: "contacto1234"
+  }
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  # Send email in development mode?
+  config.action_mailer.perform_deliveries = true
 end
