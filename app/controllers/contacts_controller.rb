10ef0f4867cd2ac43c1b37aae7f7fd562dfa06ad
Alirio Diaz <silverneonstaring@gmail.com>
2016-01-21 16:07:54 -0430

    update send email and validations

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,6 +6,7 @@ class ContactsController < ApplicationController
     if @contact.save
       ContactMailer.contact_message(params[:contact]).deliver
       redirect_to root_path, notice: 'Mensaje Enviado con Éxito'
+
     else
       render template: 'welcome/index'
     end
